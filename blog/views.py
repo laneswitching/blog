c4f6ab61ce68f3d4a5265349a07707589d5035c5
dogwqter <96355617+laneswitching@users.noreply.github.com>
2025-09-26 16:15:23 +0200

    login handling on edit

--- blog/views.py
+++ blog/views.py
@@ -35,7 +35,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.created_date = post.created_date  # Keep the original created_date  
+            post.edited_date = now() 
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
